ed70d1b06eae6e9bcdae23f3e79ea0066e9658af
Abdullah <54276938+Abdul535@users.noreply.github.com>
2022-08-08 00:22:44 +0530

    Create revise.js

---
+++ revise.js
@@ -0,0 +1,184 @@
+//---------------------------------XXX----------------------------------
+// mod: a%b => % is called mod which gives remainder 
+// modulo: a%b => b is called modulo     
+// modulus:  |-5| => 5.........Math.abs();
+// multiplication 
+// division
+    // Type Coercion
+// Bitwise 
+
+// Recursion
+// Array Helpers
+// Date Object
+// Regex
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 7:30pm
+// 9:30pm - Basics
+// 10:00pm - break
+// 1:00pm - main topics
+
+//------------------------Part 1-----------------------------
+// Basics
+    // variable alphabet,underscore(_),dollar($) shuru hota valid
+//---------------------------------XXX----------------------------------
+// Operators
+    // / - quotient
+    //     190 / 10 ----> 19.0
+    //     455 / 10 ----> 45.5
+    //     Math.floor(455/10) ----> 45
+
+    // % - remainder
+    //     190 % 10 ----> 0 
+    //     455 % 10 ----> 5 
+            // 4%8 ----> 4  RHS if greater than LHS is return
+
+// == ---->  checking value
+// === -----> value+ datatype
+    
+// Truthy values
+    // 1,2,3,4......
+    // '0'
+    // ' '
+    // " "
+    // "0"
+// Falsy values
+    // false
+    // 0
+    // ''
+    // ""
+    // []
+
+//Neither true or false
+    // null 
+    // NaN
+
+
+// logical AND - && (Find First False), (Find last true).....FFF
+// a   b   r
+// f1  f2  f1
+// f   t   f
+// t   f   f
+// t1  t2  t2
+
+// logical OR - || - Find First True, Find Last False.....FFT
+// a   b   r
+// f1  f2  f2
+// f   t   t
+// t   f   t
+// t1  t2  t1
+
+//logical not !
+
+//ternary
+// condition ? true : false
+
+
+// precedence
+// *, /  ----> what to do first
+// associativity
+// * , *  ----> what to do first
+
+// something / 0 ---> infinity
+// 1 / infinity ----> infinity
+//  infinity/ 1 ----> infinity
+// infinity / infinity ---->NaN 
+// -infinity / 0 ---->-infinity 
+// infinity / 0 ---->infinity 
+// 0 / something --- > 0
+
+
+// Bitwise operator : &, | , ~, ^, <<, >>,
+
+// & --->AND operator
+// Bitwise AND - &
+// a  b  r(a&b)
+// 0  0  0
+// 0  1  0
+// 1  0  0
+// 1  1  1
+
+
+// Bitwise OR - |
+// a  b  r(a|b)
+// 0  0  0
+// 0  1  1
+// 1  0  1
+// 1  1  1-->OR operator
+
+// ^ --->XOR operator
+// Bitwise XOR - ^
+// a  b  r(a^b)
+// 0  0  0
+// 0  1  1
+// 1  0  1
+// 1  1  0
+
+// ~ --->Negation operator
+// Bitwise NOT - ~
+// ~N = -(N+1)
+
+// << ---> LEFT shift
+// >> ---> RIGHT shift
+
+// Binary - 0bnnnn (2)
+// Decimal - nnn (10)
+// Octal - 0onnn (8)
+// Hexadecimal - 0xnnn (16)
+
+// toString
+// var a = 1000
+
+// console.log(a.toString(10))  //To convert to decimal
+// console.log(a.toString(2))  // To convert to BINARY
+// console.log(a.toString(8))  // To convert to OCTAL
+// console.log(a.toString(16))  // To convert to HEXA-DECIMAL
+
+// Unary Operator
+// +,-
+// +'string' -->  number,   if given value is 'string' then NaN
+// +number ---> positive number
+
+
+// String operator ----> +
+// jab tak + operator nai aara , normal addditon , or anything
+//     after + wala operator everything will be string
+
+//  console.log('100' + null); //'100null' 
+//  console.log(100 + null); //100
+//  console.log(8 * null); //0
+
+//---------------------------------XXX----------------------------------
+// Datatype
+
+//---------------------------------XXX----------------------------------
+
+
+// Condition
+//---------------------------------XXX----------------------------------
+// Iteration
+//---------------------------------XXX----------------------------------
+// Functions
+
+
